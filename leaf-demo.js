4644dcd8c5c57c1c93373e0468139d00014f3d75
Bratu Ionut <bratuionutz@gmail.com>
2016-11-01 21:59:12 +0200

    Adding color to the master

--- leaf-demo.js
+++ leaf-demo.js
@@ -1,44 +1,163 @@
 var map = L.map( 'map', {
-  center: [35.0, 5.0],
+  center: [46.0, 25.0],
   minZoom: 2,
-  zoom: 2
+  zoom: 7
+
 });
 
-L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  var redMarker = L.AwesomeMarkers.icon({
+    icon: 'default',
+    markerColor: 'red'
+  });
+  var greyMarker = L.AwesomeMarkers.icon({
+    icon: 'default',
+    markerColor: 'gray'
+  });
+  var greenMarker = L.AwesomeMarkers.icon({
+    icon: 'default',
+    markerColor: 'green'
+  });
+  var blueMarker = L.AwesomeMarkers.icon({
+    icon: 'default',
+    markerColor: 'blue'
+  });
+
+var iconMappings = {
+    'missingroundabout':redMarker,
+    'duplicatedways':greyMarker,
+    'orphannodes':blueMarker,
+    'orphanways':greenMarker
+  }    
+
+var assignMarkers = function (markers){
+  quarter = parseInt(markers.length/4)
+  var first = markers.slice(0,quarter)
+  var second = markers.slice(quarter,quarter*2)
+  var third = markers.slice(quarter*2,quarter*3)
+  var fourth = markers.slice(quarter*3,quarter*4)
+
+  var markerLayerMappings = {
+    'missingroundabout':first,
+    'duplicatedways':second,
+    'orphannodes':third,
+    'orphanways':fourth
+  }
+  return markerLayerMappings
+}
+
+
+
+var initMap = function(){
+  L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
   subdomains: ['a', 'b', 'c']
-}).addTo( map );
+  }).addTo( map );
+}
 
-var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
+initMap()
 
-//var myIcon = L.icon({
-//iconUrl: myURL + 'images/pin24.png',
-//  iconRetinaUrl: myURL + 'images/pin48.png',
-//  iconSize: [29, 24],
-//  iconAnchor: [9, 21],
-//  popupAnchor: [0, -14]
-//});
+var composeUrlJOSM = function(bbox){
+  var rootUrl = "http://127.0.0.1:8111/load_and_zoom?"
+  var bboxUrl = "left="+bbox.west+"&right="+bbox.east+"&top="+bbox.north+"&bottom="+bbox.south
+  return rootUrl+bboxUrl
+}
 
-//var geojsonMarkerOptions = {
-//    radius: 8,
-//    fillColor: "#ff7800",
-//    color: "#000",
-//    weight: 1,
-//    opacity: 1,
-//    fillOpacity: 0.8
-//};
+//Create for each marker and edit in Id link
+function editInID(markers, layer) {
+  var josmUrl = "http://localhost:8111/load_object?new_layer=true&objects=w"+markers.properties.id
+  var popupContent = "<p>"+ markers.properties.type+"</p>"
+  +"<p><a href="+markers.properties.url+" target='_blank'>"+"Edit in iD"+"</a>"+" | " 
+  +"<a href="+josmUrl+" target='_blank'>"+"Edit in JOSM"+"</p>";       
+  if (markers.properties && markers.properties.url) {
+    popupContent;
+  } 
+  layer.bindPopup(popupContent);
+}
 
-function onEachFeature(markers, layer) {
-		var popupContent = 
-				"<p>"+ markers.properties.id+"</p>"
-                +"<p><a href="+markers.properties.url+" target='_blank'>"+"Edit in iD"+"</p>";
-                
-if (markers.properties && markers.properties.url) {
-			popupContent;
-		}
-		layer.bindPopup(popupContent);
-        }
+var editInJOSM = function(feature,layer){
+  bbox = feature['bbox']
+  var url = composeUrlJOSM(bbox)
+  var htmlMarkup = '<p><a href ="'+url+'">Edit in JOSM</a></p>'
+  layer.bindPopup(htmlMarkup)
+}
 
-L.geoJson(markers, {
-    onEachFeature: onEachFeature
-}).addTo(map);
+var getMapBounds = function(callback){
+  map.on('dragend',function(){
+    var east = map.getBounds().getEast()
+    var west = map.getBounds().getWest()
+    var south = map.getBounds().getSouth()
+    var north = map.getBounds().getNorth()
+    bbox = {'west':west,'east':east,'south':south,'north':north}
+    callback(bbox)
+  })
+}
+
+var returnBounds = function(bbox){
+  geometries = []
+  for(key in possible_roundabouts){
+    if(possible_roundabouts.hasOwnProperty(key)){
+       possible_roundabouts[key]['bbox'] = bbox
+       geometries.push(possible_roundabouts[key]) 
+    }
+  }
+  markerMappings = assignMarkers(geometries)
+  
+}
+
+var addToMapGeoJsonLayer = function(geojson,type){
+    var geojsonLayer = L.geoJson(geojson, {
+      onEachFeature: editInID,
+      pointToLayer:function(feature,latlng){
+        return L.marker(latlng,{
+          icon:iconMappings[type]
+        });
+      }
+  })
+   
+  return geojsonLayer 
+}
+
+//var removeJsonLayer = function(mar)
+
+var clearLayers = function(){
+    map.eachLayer(function (layer) {
+    map.removeLayer(layer);
+  });
+}
+
+var clearMap = function(layers){
+  layers.forEach(function(layer){
+    map.removeLayer(layer)
+  })
+  layers = []
+}
+
+getMapBounds(returnBounds)
+map.fire('dragend')
+
+$(function(){
+   addedLayers = []
+   $("#panel").click(function(){
+     //clearLayers()
+     clearMap(addedLayers)
+     initMap()
+     checkedValues = []
+     checkboxes = $("input[type='checkbox']").each(function(){
+       if($(this).is(":checked")){
+         checkedValues.push($(this).attr("value"))
+       }
+     })
+     
+     checkedValues.forEach(function(item){
+       layer = addToMapGeoJsonLayer(markerMappings[item],item)
+       layer.addTo(map)
+       addedLayers.push(layer)
+     })
+   })
+   
+   $("#panel").trigger('click')
+})
+
+
+
+
