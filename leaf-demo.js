e57fedac8f01b16adbfa9a7e436cc7d97afe0909
Manuela Butuc <gavris_manuela1993@yahoo.com>
2016-11-03 12:30:43 +0200

    Add files via upload

--- leaf-demo.js
+++ leaf-demo.js
@@ -9,37 +9,35 @@ L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   subdomains: ['a', 'b', 'c']
 }).addTo( map );
 
-var myURL = jQuery( 'script[src$="leaf-demo.js"]' ).attr( 'src' ).replace( 'leaf-demo.js', '' );
-
-//var myIcon = L.icon({
-//iconUrl: myURL + 'images/pin24.png',
-//  iconRetinaUrl: myURL + 'images/pin48.png',
-//  iconSize: [29, 24],
-//  iconAnchor: [9, 21],
-//  popupAnchor: [0, -14]
-//});
-
-//var geojsonMarkerOptions = {
-//    radius: 8,
-//    fillColor: "#ff7800",
-//    color: "#000",
-//    weight: 1,
-//    opacity: 1,
-//    fillOpacity: 0.8
-//};
+var myIcon = L.icon({
+  iconUrl:  'images/marker.png',
+  iconSize: [23, 30],
+  iconAnchor: [9, 21],
+  popupAnchor: [0, -14]
+});
 
 function onEachFeature(possible_roundabouts, layer) {
 		var popupContent = 
 				"<p>"+ possible_roundabouts.properties.type+"</p>"
-                +"<p><a href="+possible_roundabouts.properties.url+" target='_blank'>"+"Edit in iD"+"</a>"+" | " +"<a href="+possible_roundabouts.properties.josm+" target='_blank'>"+"Edit in JOSM"+"</p>";
-                
+                +"<p><a href="+possible_roundabouts.properties.url+" target='_blank'>"+"Edit in iD"+"</a>"+" | " +"<a href="+possible_roundabouts.properties.josm+" target='_blank'>"+"Edit in JOSM"+"</p>";              
 if (possible_roundabouts.properties && possible_roundabouts.properties.url) {
 			popupContent;
 		}
 		layer.bindPopup(popupContent);
         }
 
-L.geoJson(possible_roundabouts, {
-    onEachFeature: onEachFeature
-}).addTo(map);
+   var myLayer=L.geoJson(possible_roundabouts, {
+    onEachFeature: onEachFeature,
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng,  {icon: myIcon});
+    }
+    })     
+function doalert(id){
+  if(document.getElementById(id).checked) {
+    myLayer.addTo(map);
+  }else{
+    map.removeLayer(myLayer);
+  }
+}
+
 
